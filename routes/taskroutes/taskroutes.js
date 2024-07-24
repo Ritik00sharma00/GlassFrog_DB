b5f5bf045ee6bf14a4944a463d11f2793beccf59
Ritik00sharma00 <114102148+Ritik00sharma00@users.noreply.github.com>
2024-07-24 11:51:57 +0530

    Update taskroutes.js

--- routes/taskroutes/taskroutes.js
+++ routes/taskroutes/taskroutes.js
@@ -1,13 +1,11 @@
 
-const fastifyPlugin = require('fastify-plugin');
 const taskController = require('../../controller/taskcontroller');
 
 async function taskRoutes(fastify, options) {
-  // fastify.get( '/userid', { preHandler: [fastify.authenticate] }, taskController.getUserIdByUsername)
-  fastify.post('/tasks/:userId', { preHandler: [fastify.authenticate] }, taskController.createTask);
-  fastify.get('/tasks', { preHandler: [fastify.authenticate] }, taskController.getTasks); 
-  fastify.put('/tasks/:taskId/:user_Id', { preHandler: [fastify.authenticate] }, taskController.updateTask);
-  fastify.delete('/tasks/:taskId/:user_Id', { preHandler: [fastify.authenticate] }, taskController.deleteTask);
+  fastify.post('/tasks/:userId', taskController.createTask);
+  fastify.get('/tasks', taskController.getTasks); 
+  fastify.put('/tasks/:taskId/:user_Id', taskController.updateTask);
+  fastify.delete('/tasks/:taskId/:user_Id', taskController.deleteTask);
 }
 
 module.exports = taskRoutes;
