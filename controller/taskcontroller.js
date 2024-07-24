86582a2334e7f4e21e7998f9510b837165465a86
Ritik00sharma00 <114102148+Ritik00sharma00@users.noreply.github.com>
2024-07-24 18:26:53 +0530

    Update taskcontroller.js

--- controller/taskcontroller.js
+++ controller/taskcontroller.js
@@ -65,9 +65,7 @@ async function getTasks(req, reply) {
     user_Id
   } = req.params.user_Id;
   try {
-    const userTasks = await Task.findOne({
-      user_Id
-    });
+    const userTasks = await Task.findOne({user_Id});
     if (!userTasks) {
       return reply.code(404).send({
         error: "No tasks found for this user"
