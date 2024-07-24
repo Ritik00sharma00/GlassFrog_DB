f27a4a93e4c091e91fe30e0a34e4915370bf46a1
Ritik00sharma00 <114102148+Ritik00sharma00@users.noreply.github.com>
2024-07-24 12:07:59 +0530

    Update taskroutes.js

--- routes/taskroutes/taskroutes.js
+++ routes/taskroutes/taskroutes.js
@@ -3,7 +3,7 @@ const taskController = require('../../controller/taskcontroller');
 
 async function taskRoutes(fastify, options) {
   fastify.post('/tasks/:userId', taskController.createTask);
-  fastify.get('/tasks', taskController.getTasks); 
+fastify.get('/tasks/:userId', taskController.getTasks); 
   fastify.put('/tasks/:taskId/:user_Id', taskController.updateTask);
   fastify.delete('/tasks/:taskId/:user_Id', taskController.deleteTask);
 }
